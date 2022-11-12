15c9eb54ff6a1382d668b2947926f7f9c4d272d5
monysary <sary.mony@gmail.com>
2022-11-11 20:38:27 -0800

    Change #outcome to say Good luck! When user starts the quiz, the Good luck! text will disappear. Also corrected timeAmount value when turning red because I forgot to do it on the last commit.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,25 @@ function startTimer() {
 
         if (timeAmount === 0) {
             clearInterval(timeInterval);
-        } else if (timeAmount < 117){
+        } else if (timeAmount < 30){
             timerText.setAttribute("style", "color:red; font-weight:bold;");
         };
     }, 1000);
 }
 
-// Display quiz logic, also replaced start button with submit button
+// Display quiz logic, replaces start button with submit button, hides outcome text
 var displayQuestion = document.querySelector("#questions");
 var displayAnswers = document.querySelector("#answers");
 var startButton = document.querySelector("#start-btn");
 var submitButton = document.querySelector("#submit-btn");
+var outcomeText = document.querySelector("#outcome");
 
 function startQuiz() {
     displayQuestion.setAttribute("style", "visibility:visible;");
     displayAnswers.setAttribute("style", "visibility:visible;");
     startButton.setAttribute("style", "display:none;");
-    submitButton.setAttribute("style", "display:block;")
+    submitButton.setAttribute("style", "display:block;");
+    outcomeText.setAttribute("style", "display:none;");
 };
 
 // Start button event listener for timer countdown and display quiz
