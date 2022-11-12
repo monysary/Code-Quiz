5d4f06fddcee0e26dafb43d97e88c2044cd0d9d7
monysary <sary.mony@gmail.com>
2022-11-12 10:58:13 -0800

    Updated display quiz logic to pull first object in questions array when start button is pressed. Essentially this starts the quiz by displaying the first question and its set of answer options.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,10 @@ var displayAnswers = document.querySelector("#displayed-answers");
 var startButton = document.querySelector("#start-btn");
 var submitButton = document.querySelector("#submit-btn");
 var outcomeText = document.querySelector("#outcome");
+var choices1 = document.querySelector("#choice1");
+var choices2 = document.querySelector("#choice2");
+var choices3 = document.querySelector("#choice3");
+var choices4 = document.querySelector("#choice4");
 
 // Timer countdown logic
 function startTimer() {
@@ -27,13 +31,28 @@ function startTimer() {
 // Display quiz logic, replaces start button with submit button, hides outcome text
 function startQuiz() {
     displayQuestion.textContent = questions[0].title
-    displayAnswers.textContent = questions[0].choices
+    choices1.textContent = questions[0].choices[0];
+    radioInput(choices1);
+    choices2.textContent = questions[0].choices[1];
+    radioInput(choices2);
+    choices3.textContent = questions[0].choices[2];
+    radioInput(choices3);
+    choices4.textContent = questions[0].choices[3];
+    radioInput(choices4);
 
     displayQuestion.setAttribute("style", "visibility:visible;");
     displayAnswers.setAttribute("style", "visibility:visible;");
     startButton.setAttribute("style", "display:none;");
     submitButton.setAttribute("style", "display:block;");
     outcomeText.setAttribute("style", "display:none;");
+};
+
+// Function to create radio button with text for answer choices
+function radioInput(someChoice) {
+    var radioEl = document.createElement("input");
+    radioEl.setAttribute("type", "radio");
+    radioEl.setAttribute("name", "radio-group");
+    someChoice.appendChild(radioEl);
 };
 
 // Start button event listener for timer countdown and display quiz
