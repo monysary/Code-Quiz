a1992caed14551a2e5e5c50caebd9709890f7d35
monysary <sary.mony@gmail.com>
2022-11-12 19:57:09 -0800

    Added function for submitting an answer, if answer is chosen correctly then log correct in console else log incorrect.

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,15 +32,26 @@ function startTimer() {
 // Display quiz logic, replaces start button with submit button, hides outcome text
 function startQuiz() {
     displayQuestion.textContent = questions[questionsIndex].title
-    choices1.textContent = questions[questionsIndex].choices[0];
-    radioInput(choices1);
-    choices2.textContent = questions[questionsIndex].choices[1];
-    radioInput(choices2);
-    choices3.textContent = questions[questionsIndex].choices[2];
-    radioInput(choices3);
-    choices4.textContent = questions[questionsIndex].choices[3];
-    radioInput(choices4);
 
+    // Display answer options through label tags
+    choices1.textContent = questions[questionsIndex].choices[0];
+    choices2.textContent = questions[questionsIndex].choices[1];
+    choices3.textContent = questions[questionsIndex].choices[2];
+    choices4.textContent = questions[questionsIndex].choices[3];
+
+    // Generate radio button input next to answer options
+    radioInput(choices1);
+    radioInput(choices2);
+    radioInput(choices3);
+    radioInput(choices4);
+    
+    // Set value attribute of input to answer choices 
+    displayAnswers[0].setAttribute("value", questions[questionsIndex].choices[0]);
+    displayAnswers[1].setAttribute("value", questions[questionsIndex].choices[1]);
+    displayAnswers[2].setAttribute("value", questions[questionsIndex].choices[2]);
+    displayAnswers[3].setAttribute("value", questions[questionsIndex].choices[3]);
+
+    // Display questions, answer, and submit button
     displayQuestion.setAttribute("style", "visibility:visible;");
     displayAnswers.setAttribute("style", "visibility:visible;");
     startButton.setAttribute("style", "display:none;");
@@ -52,15 +63,25 @@ function startQuiz() {
 function radioInput(someChoice) {
     var radioEl = document.createElement("input");
     radioEl.setAttribute("type", "radio");
-    radioEl.setAttribute("name", "radio-group");
+    radioEl.setAttribute("name", "answer-choice");
     someChoice.appendChild(radioEl);
 };
 
 // Submit button logic, proceeds to the next question
-function submitQuiz() {
+function submitQuiz(event) {
+    event.preventDefault();
+
+    // Check answers, proceed if correct, -5 seconds if wrong
+    if (document.querySelector("input[name='answer-choice']:checked").value === questions[questionsIndex].answer) {
+        console.log("correct");
+    } else {
+        console.log("incorrect");
+    }
+
     questionsIndex++
     startQuiz()
 };
+
 
 // Start button event listener for timer countdown and display quiz
 startButton.addEventListener("click", startTimer);
