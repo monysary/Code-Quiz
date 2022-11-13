0baae2d75f8e1025db5d96b96e8d2428919ddf8a
monysary <sary.mony@gmail.com>
2022-11-12 21:03:22 -0800

    Put everything in main element into a gride and set displayed-questions id to margin bottom 0 so displayed questions won't shift the rest of the tags in that element. New feature: timer will stop when quiz is complete.

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ function startTimer() {
         timerText.textContent = "Time: " + timeAmount + " sec";
         timerText.setAttribute("style", "font-weight:bold;");
 
-        if (timeAmount === 0) {
+        if (timeAmount === 0 || questionsIndex >= questions.length) {
             clearInterval(timeInterval);
         } else if (timeAmount < 30){
             timerText.setAttribute("style", "color:red; font-weight:bold;");
